c9afac9dda318bde071bf96257db4de8995b18b0
WasiqAli <72489247+WasiqAli@users.noreply.github.com>
2022-03-09 11:05:55 +0500

    Feat: Footer Added
    Feat: Footer Added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  Typography,
+  Button,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Container,
+  Toolbar,
+  IconButton,
+  ButtonGroup,
+} from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+import useStyles from "./style";
+
 
 function App() {
+  const classes=useStyles();
+  const card=[1,2,3,4,5,6,7,8]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant="h6">Photo Camera</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              PHOTO ALBUM
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Hello Everyone this is a Photo Album and this would be a dummy
+            </Typography>
+            <div className={classes.button}>
+            <Grid container spacing={2} justifyContent="center">
+             <Grid item>
+                <Button variant='contained' color='primary'>
+                  See my photos
+                </Button>
+             </Grid>
+             
+             <Grid item>
+                <Button variant='outlined' color='secondary'>
+                  Secondary photos
+                </Button>
+             </Grid>
+           </Grid>
+
+            </div>
+        
+          </Container>
+        </div>
+        <Container className={classes.cardGrid}  maxWidth="md" > 
+          <Grid container spacing={2}>
+           {card.map((items)=>(
+   <Grid item  xs={12} sm={6} md={4}>
+   <Card className={classes.card}>
+     <CardMedia className={classes.cardMedia}
+       image="https://source.unsplash.com/random"
+       title="Image Title"
+     />
+     <CardContent className={classes.cardContent}>
+       <Typography gutterBottom variant="h5">
+         Heading
+       </Typography>
+       <Typography>
+         This is a Card Heading Section Content
+       </Typography>
+     </CardContent>
+     <CardActions>
+       <Button size="small" color="primary">View</Button>
+       <Button size="small" color="primary">Edit</Button>
+     </CardActions>
+   </Card>
+ </Grid>
+           ))}
+
+
+           
+         
+
+          </Grid>
+
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+          <Typography variant="h6" align="center" gutterBottom>Footer</Typography>
+          <Typography variant="subtitle1" align="center"  color="textSecondary">Copyrights reserved for @ABC Company</Typography>
+      </footer>
+    </>
   );
 }
 
